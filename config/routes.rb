8c4827f50e8f8281e2811dc700d89bb8cb56e605
tanoue232 <supright34@gmail.com>
2022-10-27 00:27:54 +0900

    articleのルーティングの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # root "articles#index"
+  # get "/articles", to: "articles#index"
+  #/articles/整数 のURIのアクセスしたら、articlesコントローラのshowアクションを実行する。
+  # get "/articles/:id", to: "articles#show"
+
+  resources :articles
   resources :sample_articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
