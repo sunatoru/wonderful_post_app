53c3dddc2de7ead66034c2190759c8a4e82185b1
tanoue232 <supright34@gmail.com>
2022-11-01 21:27:58 +0900

    Deviseの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
+  # def after_sign_in_path_for(resource)
+  #   articles_path
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  # root "articles#index"
-  # get "/articles", to: "articles#index"
-  #/articles/整数 のURIのアクセスしたら、articlesコントローラのshowアクションを実行する。
-  # get "/articles/:id", to: "articles#show"
-
+  devise_for :users
   resources :articles
   resources :sample_articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
